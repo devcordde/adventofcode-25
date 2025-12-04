012bffea13a3cb1d6e46ad4d2c70d42b0e997481
Paul Hoger <paul.hoger@student.kit.edu>
2025-12-04 16:44:38 +0100

    Cleanup

--- Day-04/python/paul2708/day04.py
+++ Day-04/python/paul2708/day04.py
@@ -1,16 +1,35 @@
+from typing import List
+
 from shared.paul2708.input_reader import *
-from shared.paul2708.output import write, write_2d_array
+from shared.paul2708.output import write
 
-grid = [list(line) for line in read_plain_input(day=4, example=None)]
+grid: List[List[str]] = [list(line) for line in read_plain_input(day=4)]
 
-write_2d_array(grid)
 
-xx = 0
-change = True
-while change:
+def count_surrounding_rolls_of_paper(i: int, j: int) -> int:
+    rolls_of_paper = 0
 
-    change = False
-    diff = 0
+    for x in [-1, 0, 1]:
+        for y in [-1, 0, 1]:
+            if x == 0 and y == 0:
+                continue
+
+            new_x = i + x
+            new_y = j + y
+
+            if 0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]):
+                if grid[new_x][new_y] == "@":
+                    rolls_of_paper += 1
+
+    return rolls_of_paper
+
+
+removed_rolls = []
+removed_rolls_per_iter = -1
+
+while removed_rolls_per_iter != 0:
+    removed_rolls_per_iter = 0
+
     indices = []
 
     for i in range(len(grid)):
@@ -18,30 +37,14 @@ while change:
             if grid[i][j] != "@":
                 continue
 
-            rolls = 0
-
-            for x in [-1, 0, 1]:
-                for y in [-1, 0, 1]:
-                    if x == 0 and y == 0:
-                        continue
-
-                    a = i + x
-                    b = j + y
-
-                    if 0 <= a < len(grid) and 0 <= b < len(grid[0]):
-                        if grid[a][b] == "@":
-                            rolls += 1
-
-            if rolls < 4:
-                xx += 1
-                diff += 1
-                change = True
+            if count_surrounding_rolls_of_paper(i, j) < 4:
+                removed_rolls_per_iter += 1
                 indices.append((i, j))
 
     for a, b in indices:
         grid[a][b] = "."
 
-    print(f"Removed {diff}")
-    write_2d_array(grid)
+    removed_rolls.append(removed_rolls_per_iter)
 
-print(xx)
+write(f"After the first iteration, <{removed_rolls[0]}> rolls of paper are removed.")
+write(f"In total, <{sum(removed_rolls)}> rolls of paper are removed.")
