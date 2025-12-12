6d133d06e5a70ed658ee6c07bd6da6c2dbb8e4ab
shizophrenicgopher <aureumapes@duck.com>
2025-12-12 14:31:32 +0100

    Day Two Part One in go

---
+++ Day-02/go/artur/day02.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, _ := os.ReadFile("Day-02/go/artur/input.txt")
+	doubleIds := strings.Split(string(input), ",")
+	result := 0
+	for _, idRange := range doubleIds {
+		idSplit := strings.Split(idRange, "-")
+		id1, _ := strconv.Atoi(idSplit[0])
+		id2, _ := strconv.Atoi(idSplit[1])
+		for ; id1 <= id2; id1++ {
+			id1String := strconv.Itoa(id1)
+			if len(id1String)%2 == 1 {
+				continue
+			}
+			if id1String[:len(id1String)/2] == id1String[len(id1String)/2:] {
+				result += id1
+			}
+		}
+	}
+	println(result)
+}
